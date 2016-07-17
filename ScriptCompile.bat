d64fd2d0ddefd86a1e6431b15a5b7e70b646b1f5
UnknownPlunger <plungerman1157@gmail.com>
2016-07-17 15:05:59 -0400

    Fixed compile script

--- ScriptCompile.bat
+++ ScriptCompile.bat
@@ -1,3 +1,3 @@
-copy %2 E:\SteamGames\steamapps\common\Skyrim\Data\scripts\Sources\
-"E:\SteamGames\steamapps\common\Skyrim\Papyrus Compiler\PapyrusCompiler.exe" %1 -f="E:\SteamGames\steamapps\common\Skyrim\Papyrus Compiler\TESV_Papyrus_Flags.flg" -i=E:\SteamGames\steamapps\common\Skyrim\Data\scripts\Sources\ -o=E:\SteamGames\steamapps\common\Skyrim\Data\scripts\
+copy %2 E:\SteamGames\steamapps\common\Skyrim\Data\scripts\Source\
+"E:\SteamGames\steamapps\common\Skyrim\Papyrus Compiler\PapyrusCompiler.exe" %1 -f="E:\SteamGames\steamapps\common\Skyrim\Papyrus Compiler\TESV_Papyrus_Flags.flg" -i=E:\SteamGames\steamapps\common\Skyrim\Data\scripts\Source\ -o=E:\SteamGames\steamapps\common\Skyrim\Data\scripts\
 pause
